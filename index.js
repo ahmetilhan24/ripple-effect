38fe0694210ecf83099cc0710895ea9d1c0ee7a0
ahmetilhan24 <ahmetilhan.dev@gmail.com>
2020-12-21 01:28:54 +0300

    Project finished

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
-var button = document.getElementById("button");
+const button = document.getElementsByClassName("button");
 
-var root = document.documentElement;
+const root = document.documentElement;
 
-button.addEventListener("mousedown",  function(e){
-    var xdistance = (e.clientX - this.offsetLeft) / this.offsetWidth;
-    var ydistance = (e.clientY - this.offsetTop) / this.offsetHeight;
-    root.style.setProperty("--xdistance", xdistance);
-    root.style.setProperty("--ydistance", ydistance);
-})
+for (i = 0; i < button.length; i++){
+    button[i].addEventListener("mousedown", function(e){
+        var xdistance = (e.clientX - this.offsetLeft) / this.offsetWidth;
+        var ydistance = (e.clientY - this.offsetTop) / this.offsetHeight;
+        root.style.setProperty("--xdistance", xdistance);
+        root.style.setProperty("--ydistance", ydistance);
+    })
+}
